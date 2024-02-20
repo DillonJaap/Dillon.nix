cebf9df21666a47845cad30ffa45d459dea322f6
djaap <djaap@us.ci.org>
2024-02-19 17:56:10 -0700

    add more config

--- module/home-manager.nix
+++ module/home-manager.nix
@@ -4,12 +4,18 @@
 # add home-manager user settings here
 # generic: fzf, tree, kitty
 # derivation - like a docker image, actual build of a nix build command
-	home.packages = with pkgs; [ git neovim ocaml go kitty opam];
+
+# commands for building / check
+# darwin-rebuild check --flake ".#aarch64"
+# darwin-rebuild build --flake ".#aarch64"
+	home.packages = with pkgs; [ git neovim ocaml go kitty];
 	home.stateVersion = "23.11";
 
 	programs = {
+
 		kitty = {
 			enable = true; 
+			extraConfig  = builtins.readFile ../config/kitty/kitty.conf;
 		};
 	};
 }
