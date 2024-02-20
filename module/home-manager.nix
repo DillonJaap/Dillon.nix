e45cc9cf260a838e43008064743f3364d4d8a1f9
djaap <djaap@us.ci.org>
2024-02-19 17:34:15 -0700

    add packages

--- module/home-manager.nix
+++ module/home-manager.nix
@@ -3,7 +3,8 @@
 {
 # add home-manager user settings here
 # generic: fzf, tree, kitty
-	home.packages = with pkgs; [ git neovim ];
+# derivation - like a docker image, actual build of a nix build command
+	home.packages = with pkgs; [ git neovim ocaml go kitty opam];
 	home.stateVersion = "23.11";
 
 	programs = {
