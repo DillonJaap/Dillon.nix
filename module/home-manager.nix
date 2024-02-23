d5b0533daee670d1bac2e8996df1be4d1dc7960d
djaap <djaap@us.ci.org>
2024-02-22 17:40:55 -0700

    update

--- module/home-manager.nix
+++ module/home-manager.nix
@@ -8,11 +8,10 @@
 # commands for building / check
 # darwin-rebuild check --flake ".#aarch64"
 # darwin-rebuild build --flake ".#aarch64"
-	home.packages = with pkgs; [ git neovim ocaml go kitty];
+	home.packages = with pkgs; [git neovim ocaml go odin gcc];
 	home.stateVersion = "23.11";
 
 	programs = {
-
 		kitty = {
 			enable = true; 
 			extraConfig  = builtins.readFile ../config/kitty/kitty.conf;
