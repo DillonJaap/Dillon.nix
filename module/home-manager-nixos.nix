d5db9a8bc29882e501e637ee64e2793dda9fd41f
djaap <djaap@us.ci.org>
2024-03-15 16:31:35 -0600

    add indivdual configs

--- module/home-manager-nixos.nix
+++ module/home-manager-nixos.nix
@@ -5,13 +5,6 @@ let
 in
 {
 # add home-manager user settings here
-# generic: fzf, tree, kitty
-# derivation - like a docker image, actual build of a nix build command
-
-# commands for building / check
-# darwin-rebuild check --flake ".#aarch64"
-# darwin-rebuild build --flake ".#aarch64"
-# darwin-rebuild switch --flake ".#aarch64"
 	imports = [
 		./home-manager-shared.nix
 	];
