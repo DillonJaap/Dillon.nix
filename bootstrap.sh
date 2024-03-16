d33c094a29ece39552cf27931b09efe308638197
djaap <djaap@us.ci.org>
2024-03-16 10:52:59 -0600

    update

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,6 +2,8 @@
 
 nix-shell -p git skate
 mkdir -p $HOME/.ssh
+skate link
+skate sync
 skate get "github public key"@keys > $HOME/.ssh/id_ed25519.pub
 skate get "github private key"@keys > $HOME/.ssh/id_ed25519
 exit
