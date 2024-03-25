1bef9cfd6b60e077caada4cc8804008ddf5c7e89
dillon <dillonjaap@gmail.com>
2024-03-25 12:15:49 -0600

    update

--- module/home-manager-nixos.nix
+++ module/home-manager-nixos.nix
@@ -32,4 +32,10 @@ in
 			xrandr --addmode Virtual-1 3440x1440_60.00
 		'';
 	};
+
+	programs.bash.profileExtra = ''
+			xrandr --newmode "3440x1440_60.00"  419.50  3440 3696 4064 4688  1440 1443 1453 1493 -hsync +vsync
+			xrandr --addmode Virtual-1 3440x1440_60.00
+			xrandr --output Virtual-1 --primary --mode 3440x1440_60.00 --pos 0x0 --rotate normal
+		'';
 }
